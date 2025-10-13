d6ec0c87c59b8e51417c9e304778a43e4bae6b3a
Diogo Xavier <150879008+kasuki0089@users.noreply.github.com>
2025-10-13 22:31:10 +0000

    teste

--- hall-of-jerseys-next15/src/app/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding: 20}}>
-      <h1>Hall of Jerseys - Home</h1>
-      <p>Projeto TCC - Next.js 15 skeleton</p>
-    </main>
-  );
-}
