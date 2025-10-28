58535fcfb91021d882cd41b260d40b5883b88d16
Daniel <danieleandronataly@gmail.com>
2025-10-28 19:16:56 -0300

    Criação do footer e a lógica de main template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-import BestSellers from "@/components/BestSellers"
-import Carousel from "@/components/Carousel"
-import Galery from "@/components/Galery"
-import Navbar from "@/components/NavBar"
+import BestSellers from "@/components/BestSellers";
+import Carousel from "@/components/Carousel";
+import Galery from "@/components/Galery";
+import MainTemplate from "@/templates/MainTemplate/Index";
 
 export default async function HomePage() {
-    return (
-        <>
-            <Navbar/>
-            <Carousel/>
-            <Galery/>
-            <h2 className="text-center text-xl font-semibold mt-8 text-gray-800">Mais vendidos</h2>
-            <BestSellers/>
-            <h2 className="text-center text-xl font-semibold mt-8  text-gray-800">Lançamentos</h2>
-            <BestSellers/>
-        </>
-    )
+  return (
+    <>
+      <MainTemplate>
+        <Carousel />
+        <Galery />
+        <h2 className="text-center text-xl font-semibold mt-8 text-gray-800">
+          Mais vendidos
+        </h2>
+        <BestSellers />
+        <h2 className="text-center text-xl font-semibold mt-8  text-gray-800">
+          Lançamentos
+        </h2>
+        <BestSellers />
+      </MainTemplate>
+    </>
+  );
 }
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,29 +1,35 @@
-'use client';   
+'use client';
 import { Search, ShoppingCart, User } from "lucide-react";
 import Image from "next/image";
 import logomarca from "@/public/images/logomarca.jpeg";
 
 export default function Navbar() {
-  return (  
-    <nav className="w-full bg-primary text-white py-3 px-8 flex items-center justify shadow-md">
-      <div className="flex items-center">
-        <div className="w-40 h-10 flex items-center justify-center">
-          <Image src={logomarca} alt="Logomarca" className="w-full h-full object-contain"/>
+  return (
+    <nav className="w-full bg-primary text-white py-3 shadow-md">
+      <div className="max-w-8xl mx-auto px-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="w-40 h-10 flex items-center">
+            <Image
+              src={logomarca}
+              alt="Logomarca"
+              className="w-full h-full object-contain"
+            />
+          </div>
         </div>
-      </div>
 
-      <ul className="hidden md:flex items-center gap-7 font-light text-sm ml-[50vw]">
-        <li className="hover:text-secondary cursor-pointer transition duration:500">PRODUTOS</li>
-        <li className="hover:text-secondary cursor-pointer transition duration:500">SOBRE</li>
-        <li className="hover:text-secondary cursor-pointer transition duration:500">CONTATO</li>
-        <li className="hover:text-secondary cursor-pointer transition duration:500">SUPORTE</li>
-      </ul>
+        <ul className="hidden md:flex items-center gap-7 font-light text-sm">
+          <li className="hover:text-secondary cursor-pointer transition duration-500">PRODUTOS</li>
+          <li className="hover:text-secondary cursor-pointer transition duration-500">SOBRE</li>
+          <li className="hover:text-secondary cursor-pointer transition duration-500">CONTATO</li>
+          <li className="hover:text-secondary cursor-pointer transition duration-500">SUPORTE</li>
+        </ul>
 
-      <div className="flex items-center gap-4 ml-[12vw]">
-        <Search className="w-5 h-5 cursor-pointer hover:text-secondary transition duration-500" />
-        <ShoppingCart className="w-5 h-5 cursor-pointer hover:text-secondary transition duration-500" />
-        <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:scale-105 transition-transform duration-500">
-          <User className="w-4 h-4 text-secondary" />
+        <div className="flex items-center gap-4">
+          <Search className="w-5 h-5 cursor-pointer hover:text-secondary transition duration-500" />
+          <ShoppingCart className="w-5 h-5 cursor-pointer hover:text-secondary transition duration-500" />
+          <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center cursor-pointer hover:scale-105 transition-transform duration-500">
+            <User className="w-4 h-4 text-secondary" />
+          </div>
         </div>
       </div>
     </nav>
